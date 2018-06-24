e376a60e218e2fff461147ca3c5f30fc7edf08f8
Silvio Mori Neto <silviomori@gmail.com>
2018-06-24 12:31:40 -0300

    Fetching categories after loading CategoriesPage.

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CategoriesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { CategoryService } from '../../services/domain/category.service';
 
 @IonicPage()
+
 @Component({
   selector: 'page-categories',
   templateUrl: 'categories.html',
 })
 export class CategoriesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public categoryService: CategoryService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriesPage');
+    this.categoryService.fetchAll()
+      .subscribe(
+        response => { console.log(response) },
+        error => { console.log(error) }
+      );
   }
 
 }
