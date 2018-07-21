2bfb75aaaa1568ab01533e50ae650306854d510f
Silvio Mori Neto <silviomori@gmail.com>
2018-07-21 20:20:50 -0300

    Getting an image from gallery to set as profile picture.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -83,6 +83,29 @@ export class ProfilePage {
     );
   }
 
+  getGalleryPicture() {
+    
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    };
+
+    this.camera.getPicture( options ).then(
+      (imageData) => {
+        this.picture = 'data:image/png;base64,' + imageData;
+        this.cameraOn = false;
+      },
+      (err) => {
+        this.cameraOn = false;
+      }
+    );
+  }
+
   sendProfilePicture() {
     this.customerService.uploadProfilePicture( this.picture )
       .subscribe(
