3a92eb4438dfc1aafc9546ffcfc0c31387306d06
Silvio Mori Neto <silviomori@gmail.com>
2018-07-21 18:51:30 -0300

    First test using an Android smartphone

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://silviomori-order-manager.herokuapp.com",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/order-manager"
 }
